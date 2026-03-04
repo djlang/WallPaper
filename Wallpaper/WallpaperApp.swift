8d3ab9ddce7845a8902cf8c1c630d47614b4ba3e
dengjinlang <dengjinlang@aiiiin.com>
2026-03-04 15:14:40 +0800

    Initial Commit

---
+++ Wallpaper/WallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpaperApp.swift
+//  Wallpaper
+//
+//  Created by dj on 2026/3/4.
+//
+
+import SwiftUI
+
+@main
+struct WallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
